8171831a5cee4a3e9728f6768190215c59972228
VALEUR Virginie <valeurvirginie@macbook-pro-de-valeur.home>
2018-10-24 17:45:37 +0200

    Base de donnée du Blog

---
+++ blog_des_familles/db/seeds.rb
@@ -0,0 +1,12 @@
+require 'faker'
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do
+ like = Like.create(article_id: Faker::Number.between(11, 20), user_id: Faker::Number.between(11, 20))
+end
